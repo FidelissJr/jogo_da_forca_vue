36520413117137cda7e2e5cbf5ae78246cea893d
Andre Fidelis <andre.fidelis@linx.com.br>
2025-07-08 18:35:40 -0300

    Ajuste tamanho letras

--- src/components/Palavra.vue
+++ src/components/Palavra.vue
@@ -52,6 +52,19 @@ export default {
   align-items: center;
 }
 
+/* Para telas pequenas (celulares) */
+@media (max-width: 768px) {
+  .palavra-letra {
+    height: 15px;
+    width: 15px;
+    margin: 0 5px;
+    border-bottom: 1px solid var(--color-text-light);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
 .espaco {
   margin: 0 12px 0 12px;
 }
